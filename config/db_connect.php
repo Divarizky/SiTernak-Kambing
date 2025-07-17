cbd33ede5cda3f11a2fdb2f2250c61857e5a7864
Divarizky <divarizky28@gmail.com>
2025-07-17 10:00:47 +0700

    add: menambahkan konfigurasi koneksi database di db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,8 +1,15 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "peternakan_kambing";
+// Konfigurasi database untuk deploy
+// $host = "sql311.infinityfree.com";
+// $user = "if0_39487263";
+// $pass = "XX7NGiAkS9";
+// $db   = "if0_39487263_peternak_kambing";
+
+// Local
+$host = "localhost"; // default: localhost
+$user = "root"; // default: root
+$pass = ""; // default: ""
+$db   = "peternakan_kambing"; // default: peternak_kambing
 
 $koneksi = new mysqli($host, $user, $pass, $db);
 
